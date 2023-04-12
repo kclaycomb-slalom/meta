f9bd73277f727a14ff75694191f23f54f75c7e15
Kirk Larsen <kirkl@slalom.com>
2023-04-11 20:55:05 -0600

    I think ssh is good, working on ansible ssh'ing in

--- terraform/ec2/ansible_inventory.tf
+++ terraform/ec2/ansible_inventory.tf
@@ -1,6 +1,9 @@
 resource "local_file" "ansible_inventory" {
   filename = "../../ansible/inventory/hosts.ini"
-  content  = <<EOF
+  #  lifecycle {
+  #    ignore_changes = content
+  #  }
+  content = <<EOF
 [meta]
 "${module.ec2_instance.public_ip}"
 EOF
--- terraform/ec2/user-key.tf
+++ terraform/ec2/user-key.tf
@@ -1,10 +1,12 @@
-resource "aws_iam_user_ssh_key" "user" {
-  username   = "AWS-InnovationLabs-SF-Admin/kirkl@slalom.com"
-  encoding   = "SSH"
-  public_key = local.ssh_public_key
-}
+
+# NOTE: can't use this because Slalom's federated username contains "/"
+#resource "aws_iam_user_ssh_key" "user" {
+#  username   = "AWS-InnovationLabs-SF-Admin/kirkl@slalom.com"
+#  encoding   = "SSH"
+#  public_key = local.ssh_public_key
+#}
 
 resource "aws_key_pair" "generated_key" {
   key_name   = "${local.extracted_user}-key"
-  public_key = aws_iam_user_ssh_key.user.public_key
+  public_key = local.ssh_public_key
 }
