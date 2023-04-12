1215c5e5c26d2e3a9a9d05c1b53e89883c4d828e
Kirk Larsen <kirkl@slalom.com>
2023-04-12 17:55:12 -0600

    reduce errors

--- terraform/ec2/ec2.tf
+++ terraform/ec2/ec2.tf
@@ -1,21 +1,29 @@
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+  source = "../modules/terraform-aws-ec2-instance"
+  #version = "~> 3.0"
 
   name = "meta-onboarding-${data.aws_caller_identity.current.user_id}"
 
-  ami           = var.ami
-  instance_type = var.ec2type
-  key_name      = aws_key_pair.generated_key.key_name
-  monitoring    = true
-  subnet_id     = data.aws_subnet.subnet_target.id
-  root_block_device = {
-    encrypted = true
-  }
-  metadata_options = {
-    http_tokens = "required"
-  }
+  ami                    = var.ami
+  instance_type          = var.ec2type
+  key_name               = aws_key_pair.generated_key.key_name
+  monitoring             = true
+  subnet_id              = data.aws_subnet.subnet_target.id
+  ebs_optimized          = true
+  vpc_security_group_ids = [aws_security_group.meta-sg.id]
 
+  root_block_device = [
+    {
+      device_name = "/dev/xvda"
+      volume_type = "gp2"
+      volume_size = 8
+      encrypted   = true
+    }
+  ]
+  metadata_options = {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
   tags = {
     Terraform   = "true"
     Environment = "dev"
